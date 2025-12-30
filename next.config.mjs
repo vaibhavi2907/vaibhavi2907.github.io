b658b463a49aa801f1cf0bceb089e798b03753e3
vaibhavi2907 <vaibhavilawhale@gmail.com>
2025-12-30 23:26:59 +0530

    updated next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: "https://vaibhavi2907.github.io/",
+  assetPrefix: "https://vaibhavi2907.github.io/",
   images: {
     unoptimized: true,
   },
